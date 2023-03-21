b3a4eb4401ae52b950a8d84e02e2faa1209cd9c9
peirong wang <peirongw@amazon.com>
2023-03-21 21:28:53 +0800

    update

--- bin/cdk-project.ts
+++ bin/cdk-project.ts
@@ -19,7 +19,7 @@ export class MyPipelineStack extends Stack {
                 input: CodePipelineSource.gitHub('wpr7280/cdk_project', 'pipeline_01', {
                     authentication: SecretValue.secretsManager('githubToken'),
                 }),
-                commands: ['npm ci', 'env NPM_CONFIG_UNSAFE_PERM=true npx cdk synth',]
+                commands: ['npm ci', 'npm uninstall -g aws-cdk', 'npm install -g aws-cdk', 'env NPM_CONFIG_UNSAFE_PERM=true npx cdk synth',]
             })
         });
     }
