3f7fa41196c3ad795e5c5c72bf543250eae332b8
peirong wang <peirongw@amazon.com>
2023-03-21 20:29:06 +0800

    test

--- bin/cdk-project.ts
+++ bin/cdk-project.ts
@@ -7,19 +7,22 @@ import {
 } from "aws-cdk-lib";
 import {CodePipeline, CodePipelineSource, ShellStep} from "aws-cdk-lib/pipelines";
 import {Construct} from "constructs";
+
 const app = new App();
+
 export class MyPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props);
         const pipeline = new CodePipeline(this, 'MyCDKPipeline', {
             pipelineName: 'MyCDKPipeline',
             synth: new ShellStep('Synth', {
-                input: CodePipelineSource.gitHub('wpr7280/cdk_project', 'pipeline_01',{
-                    authentication:  SecretValue.secretsManager('githubToken'),
+                input: CodePipelineSource.gitHub('wpr7280/cdk_project', 'pipeline_01', {
+                    authentication: SecretValue.secretsManager('githubToken'),
                 }),
-                commands: ['npx cdk synth']
+                commands: ['npm ci', 'npm run build', 'npx cdk synth']
             })
         });
     }
 }
+
 new MyPipelineStack(app, 'MyCDKPipelineStack');
